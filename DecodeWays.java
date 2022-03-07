1bb6ba2a659cb784466e59266a1d988c4af33ce9
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-06 16:26:25 -0800

    Create DecodeWays.java

---
+++ DecodeWays.java
@@ -0,0 +1,21 @@
+class DecodeWays {
+    //https://leetcode.com/problems/decode-ways/submissions/
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0]=1;
+        dp[1]= s.charAt(0) == '0' ? 0 : 1;
+        
+        for(int i=2;i<s.length()+1;i++){
+            if(s.charAt(i-1) !='0'){
+                dp[i] = dp[i-1];
+            }
+            
+            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            if(twoDigit>=10 && twoDigit<=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
