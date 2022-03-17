3adfeba4ff92800ddf47c5f36ca5e37b852062c9
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-17 15:29:21 -0700

    Create BuyAndSellStockWithTransactionFee.java

---
+++ BuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,32 @@
+class BuyAndSellStockWithTransactionFee {
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solution/
+    int[] prices;
+    int[][]memo;
+    int fee;
+    public int maxProfit(int[] prices, int fee) {
+        this.prices = prices;
+        this.fee = fee;
+        memo = new int[prices.length][2];
+        
+        return dp(0,0);
+    }
+    
+    private int dp(int day, int holding){
+        if(day >= this.prices.length){
+            return 0;
+        }
+        
+        if(memo[day][holding] == 0){
+            int doNothing = dp(day + 1, holding);
+            int doSomething;
+            if(holding == 1){
+                doSomething = -this.fee + this.prices[day] + dp(day+1, 0) ;
+            }else{
+                doSomething = -this.prices[day] + dp(day+1, 1) ;
+            }
+            memo[day][holding] = Math.max(doNothing,doSomething);
+        }
+        
+        return memo[day][holding];
+    }
+}
