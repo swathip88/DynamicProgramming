99b588ea1b568b8dd7dafa1f8618a6c53d06196b
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-17 15:45:05 -0700

    Update BuyAndSellStockWithTransactionFee.java

--- BuyAndSellStockWithTransactionFee.java
+++ BuyAndSellStockWithTransactionFee.java
@@ -29,4 +29,19 @@ class BuyAndSellStockWithTransactionFee {
         
         return memo[day][holding];
     }
+    
+    
+    
+}
+
+//Simplest solution
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
 }
