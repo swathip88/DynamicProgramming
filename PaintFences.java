e1ae39e87ca4db43f9a3f928c47b2d33bc6ac54b
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-16 19:33:17 -0700

    Create PaintFences.java

---
+++ PaintFences.java
@@ -0,0 +1,15 @@
+class PaintFences {
+    //https://leetcode.com/problems/paint-fence/submissions/
+    public int numWays(int n, int k) {
+        if(n == 1) return k;
+        if(n==2) return k*k;
+        int[] totalWays = new int[n+1];
+        totalWays[1] = k;
+        totalWays[2] = k * k;
+        for(int i=3;i<=n;i++){
+            totalWays[i] = (k-1) * (totalWays[i-1] + totalWays[i-2]);
+        }
+        
+        return totalWays[n];
+    }
+}
